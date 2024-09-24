703bbe0016caad729c3b2843c2a246522ed7bb28
jobex11 <joblawal33@gmail.com>
2024-09-24 14:18:10 +0100

    fix bugs on build vite

--- database/index.js
+++ database/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 // added connection starts
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 //connections ends
-
 
 mongoose
   .connect(
