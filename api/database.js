2363c336460e6e9552683e41fa0ff7a7fc96f9ef
jobex11 <joblawal33@gmail.com>
2024-09-24 17:36:52 +0100

    made changes

--- api/database.js
+++ api/database.js
@@ -2,6 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Create __dirname and __filename in ES module environment
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
